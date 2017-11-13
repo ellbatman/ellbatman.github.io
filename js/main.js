074680175179e6d62e348ac3bb8c99b856786c92
Luke Bateman <bateman.w.luke@gmail.com>
2017-11-12 19:32:38 -0500

    ties now

---
+++ js/main.js
@@ -0,0 +1,257 @@
+var developers = [
+	//Luke Bateman
+	{
+		id: 'luke',
+		name: 'Luke Bateman',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+	//Lei Luo
+	{
+		id: 'lei',
+		name: 'Lei Luo',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+	//Lilit Balagyozyan
+	{
+		id: 'lilit',
+		name: 'Lilit Balagyozyan',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+	//Sean Alderdice
+	{
+		id: 'sean',
+		name: 'Sean Alderdice',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+	//Emil Andreasan
+	{
+		id: 'emil',
+		name: 'Emil Andreasan',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+	//Jack Yang
+	{
+		id: 'jack',
+		name: 'Jack Yang',
+		profilePic: 'url(images/projects/senstaff/prototype1.jpg)',
+		linkedIn: 'https://www.linkedin.com/in/luke-bateman-245226a0/',
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quae inventore fuga similique accusamus soluta quibusdam nostrum veniam minima, laboriosam numquam, quis deleniti velit id porro maxime nobis. Voluptatum, nulla.'
+	},
+
+]
+var threads = [
+	//senstaff
+	{
+		id: 'senstaff',
+		title: 'The Senstaff',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgsTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	//doorstop
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+	{
+		id: 'doorstop',
+		title: 'PDI Doorstop',
+		subTitle: 'I am a subtitle!',
+		mainImg: 'url(images/projects/senstaff/prototype1.jpg)',
+		org: 'Programs in Design & Innovation',
+		cat: 'Product',
+		imgs: ['url(images/projects/senstaff/prototype1.jpg)',
+		'url(images/projects/senstaff/working.jpg)'],
+		imgTxt: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
+		info: ['rit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'icia deserunt mollit anim id est laborum.', 'laborum.'],
+		link: 'https://drive.google.com/open?id=0B32gQGnV1_U-UVdaT0tockxDQUk',
+		team: ['luke', 'emil', 'jack']
+	},
+
+];
+//Creates threads equal to the amount stored in the threads array and assigns them ids associated with their stored id variable
+for (i = 0; i < threads.length; i++) {
+	$('.threads').append('<div class="thread"><a rel="leanModal" onclick="populateModal($(this)); return true;" class="threadLink mainImg" href="#modalPopup"></a><div class="threadDetails"><a rel="leanModal" onclick="populateModal($(this)); return true;" class="threadLink threadTitle" href="#modalPopup"></a><a rel="leanModal" onclick="populateModal($(this)); return true;" class="threadLink threadOrg" href="#"></a><div></div><a rel="leanModal" onclick="populateModal($(this)); return true;" class="threadLink threadCat" href="#"></a></div></div>');
+
+	$('.thread:nth-of-type('+(i+1)+')').addClass(threads[i].id).attr('id', i);
+	$('.thread .mainImg').css('background-image', threads[i].mainImg);
+	$('.thread .threadCat').html(threads[i].cat);
+	$('.thread .threadTitle').html(threads[i].title);
+	$('.thread .threadOrg').html(threads[i].org);
+	console.log(threads[i].id);
+}
+
+
+$(window).scroll(function (event) {
+	var distanceScrolled = $(window).scrollTop();
+	if (distanceScrolled > 240) {
+		$('.navBar').addClass('stickyNav');
+	} else {
+		$('.navBar').removeClass('stickyNav');
+
+	}
+});
+
+
+
+
+
+// KONAMI Code!
+/*
+var kkeys = [];
+var konami = "38,38,40,40,37,39,37,39,66,65";
+
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
+
+    // Add your own easter egg here!!
+    $('canvas').fadeIn('fast');
+  }
+});
+/*
+$('.projectMenu li:nth-child(1)').hover(function () {
+	$('.aboutUs').show();} , function () {
+		$('.aboutUs').hide();
+});
+
+$('.organizeMenu li:nth-child(2) h2').hover(function () {
+	$('.ourOrganization').show();} , function () {
+		$('.ourOrganization').hide();
+});
+
+
+/* On scroll window function
+$(window).on('scroll',function(){
+	var scrollposition = $(window).scrollTop();
+
+	var var1 =
+	$('body').css('background','linear-gradient(to bottom, rgb('+var1+','+var2+','+var3+'))');
+});
+*/
