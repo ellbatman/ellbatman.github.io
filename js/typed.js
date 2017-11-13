aa95ab9d795ba9d334e3bbb18c68a7a4a83e0875
Luke Bateman <bateman.w.luke@gmail.com>
2017-11-12 22:54:08 -0500

    discover bar

--- js/typed.js
+++ js/typed.js
@@ -1,6 +1,6 @@
 var headerTxt = new Typed('.headerTxt', {
     contentType: 'text',
-    strings: ['We Are Building Community', 'We Are Sustaining Initiatives', 'We Are Exploring Alternatives', 'We Are TIES', 'Technology, Innovation, Entrepreneurship, & Sustainability'],
+    strings: ['We Are Building Communities', 'We Are Sustaining Initiatives', 'We Are Exploring Alternatives', 'We Are TIES', 'Technology, Innovation, Entrepreneurship, & Sustainability'],
     typeSpeed: 50,
     backSpeed: 10,
     smartBackspace: true,
@@ -8,14 +8,17 @@ var headerTxt = new Typed('.headerTxt', {
     loop: false,
 });
 
-// var searchTxt = new Typed('#searchTxt', {
-//     strings: ['Discover new ideas', 'Discover innovative projects', 'Discover local organizations', 'Discover creative people'],
-//     typeSpeed: 50,
-//     backSpeed: 0,
-//     smartBackspace: true,
-//     backDelay: 500,
-//     loop: true,
-//     loopCount: Infinity,
-//     attr: 'placeholder',
-//     bindInputFocusEvents: true,
-// });
+$('#searchTxt').hover(function(){
+  var searchTxt = new Typed('#searchTxt', {
+      strings: ['', 'Discover Interesting Problems', 'Generate New Ideas', 'Build Interdisciplinary Teams', 'Advance Innovative Projects', 'Support Local Organizations', 'Empower Sustainable Development', 'Discover'],
+      typeSpeed: 30,
+      backSpeed: 0,
+      smartBackspace: true,
+      backDelay: 500,
+      loop: false,
+      loopCount: Infinity,
+      attr: 'placeholder',
+      bindInputFocusEvents: true,
+  });
+  $('#searchTxt').removeData();
+});
